1516c09178c0c6985d8cd5f454bb92a7ab879114
= <=>
2014-10-15 13:27:23 +0100

    Extract Blue Jay Feather Data

--- extract.py
+++ extract.py
@@ -30,10 +30,14 @@ def extract(imageFile,bar,pixels):
     image = misc.imread(imageFile)
     print(image.shape)
 
+    image = quantize(image)
+
     transform = invfourier(image)
 
     pi = np.pi
     size = image.shape[0]
+    if size < 200:
+        return None
 
     q,f = imbin(np.abs(transform))
     a = invabel(f)
@@ -45,9 +49,12 @@ def extract(imageFile,bar,pixels):
     print(scale)
     print(size)
     print(scale*size)
+    index = 1.54
+    fraction = 0.0
+    index = 1 + fraction * (index-1)
     
-    wave = scale*size/(q+1)
-    mask = np.logical_and(wave > 200, wave < 1000)
+    wave = 2*scale*size/(q+1)*index
+    mask = np.logical_and(wave > 40, wave < 1000)
 
     return (wave[mask],a[mask]*scale)
 
@@ -56,15 +63,18 @@ def plotSample(cur,index):
     rows = cur.fetchall()
     hasPlot = False
     accum = []
-    xs = np.arange(250,950)
+    xs = np.arange(350,750)
     for i,p,b,n in rows:
+        values = extract(i,b,p)
+        if values is None:
+            continue
+        q,a = values
         hasPlot = True
         print(i)
-        q,a = extract(i,b,p)
         accum.append(scipy.interp(xs,q[::-1],a[::-1]))
         #plt.plot(q,a,label=i)
     if hasPlot:
-        average = np.mean(np.vstack(accum),axis=0)
+        average = np.median(np.vstack(accum),axis=0)
         #plt.plot(xs,average,label="Average")
         #plt.title(n)
         #plt.legend()
@@ -81,20 +91,15 @@ if __name__=="__main__":
         cur.execute('SELECT "index" FROM samples;')
         indices = cur.fetchall()
 
+        indices = [(7,)]
+
         values = [plotSample(cur,index[0])
                  for index in indices]
         values = [v for v in values if v is not None]
-        xs = np.arange(250,950)
+
+        xs = np.arange(350,750)
         for title,value in values:
             plt.plot(xs,value,label=title)
+            np.savetxt("Abeled.txt",np.vstack([xs,value]).T)
         plt.legend()
         plt.show()
-
-        # cur.execute('SELECT "image","pixels","bar","name" FROM images INNER JOIN samples ON "index" == "sample";')
-        # rows = cur.fetchall()
-        # for i,p,b,n in rows:
-        #     print(i)
-        #     q,a = extract(i,b,p)
-        #     plt.plot(q,a,label=n)
-        # plt.legend()
-        # plt.show()
